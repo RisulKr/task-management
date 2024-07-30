de3fe404968342831657d4ad301eba1a85e02f35
To'lqin Oltiboyev <tolking0101@gmail.com>
2024-07-30 12:15:17 +0500

    FIX TaskDTO builder problem

--- src/main/java/com/epam/internship/dto/TaskSelectDTO.java
+++ src/main/java/com/epam/internship/dto/TaskSelectDTO.java
@@ -2,11 +2,15 @@ package com.epam.internship.dto;
 
 import com.epam.internship.enums.Priority;
 import com.epam.internship.enums.Status;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TaskSelectDTO {
     private long id;
     private String title;
